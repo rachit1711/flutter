1a969aeb4ce0663142869407ec039390e68a8458
Rachit Narang <rachitnarang1711@gmail.com>
2021-02-18 23:03:12 +0530

    day 2 of flutter

---
+++ lib/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class Homepage extends StatelessWidget {
+  final int days=30;
+  final name="rachit";
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: new AppBar(
+        title: Text("Catalog app"),
+      ),
+      body:Center(
+        child: Container(
+          child: new Text("welcome to $days days of flutter by $name"),
+        ),
+
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'homepage.dart';
 void main()
 {
   runApp(new MyApp());
@@ -6,16 +7,20 @@ void main()
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double days=30;
+    String name="Rachit";
+    double pi=3.14;
+    bool isMale=true;
+    //both int and double will be used as num
+    num temp=30;
+    num temp1=30.5;
+
+    //any value can be commonly used as var
+    var day="tuesday";     //THE MOST COMMON DATATYPE IN DART
+    const pi1 =3.14; //here data cannot be modified
+    //is used
     return MaterialApp(
-      home: Material(
-        child:Center(
-          child: Container(
-            child: new Text("welcome to 30 days of flutter"),
-          ),
-
-        ),
-
-      ),
+      home:Homepage()
     );
   }
 }
