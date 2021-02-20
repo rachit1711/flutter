76100f4aa2e4cec6ec63ea17966ddd572a285fbf
Rachit Narang <rachitnarang1711@gmail.com>
2021-02-20 13:48:40 +0530

    day 6 of flutter stateful widgets animated container ,box decoration container doesnt have a property of click button as elevatedbutton so either wrap it with gesture detector or with inkwell inkwell uses onTap() instead of onPressed()
    use of conditional statement with animation
    
    future delay property (await)

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
-  final String name = "Codepur";
+  final String name = "Rachit Narang";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +11,10 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-          child: Text("Welcome to $days days of flutter by $name"),
+          child: Text("Hello $name",style: new TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),),
         ),
       ),
       drawer: Drawer(),
--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name="";
+  // _ underscore makes a var/func /class private in dart
+  bool changeButton=false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,7 +26,7 @@ class LoginPage extends StatelessWidget {
                 height: 20.0,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
@@ -37,6 +45,13 @@ class LoginPage extends StatelessWidget {
                         hintText: "Enter username",
                         labelText: "Username",
                       ),
+                      onChanged: (value){
+                        name=value;
+                        setState(() {
+                          //setState is a method used to change our UI by calling the build method again\
+                          //it works only with stateful widget
+                        });
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -48,14 +63,39 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 40.0,
                     ),
-                    ElevatedButton(
-                      child: Text("Login"),
-                      style: TextButton.styleFrom(minimumSize: Size(150,40)),
 
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.HomeRoute);
+                    InkWell(
+                      onTap: ()
+                      async {
+                        setState(() {
+                          changeButton=true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+                       Navigator.pushNamed(context, MyRoutes.HomeRoute);
                       },
-                    )
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton?50:150,
+                        height: 50,
+                        //color: Colors.deepPurple,
+                        alignment: Alignment.center,
+                        child: changeButton?Icon(Icons.done,color: Colors.white,):Text("Login",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 18),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          //shape: changeButton?BoxShape.circle:BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(changeButton?50:8)
+                        ),
+                      ),
+                    ),
+                    // ElevatedButton(
+                    //   child: Text("Login"),
+                    //   style: TextButton.styleFrom(minimumSize: Size(150,40)),
+                    //
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.HomeRoute);
+                    //   },
+                    // )
                   ],
                 ),
               )
